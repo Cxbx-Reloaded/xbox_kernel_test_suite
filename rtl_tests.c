99e441ea6083246f587310c183c404d13cfb1537
Fisherman166 <Seankoppenhafer@gmail.com>
2018-02-26 20:40:04 -0800

    Add test for RtlInitAnsiString

--- rtl_tests.c
+++ rtl_tests.c
@@ -157,7 +157,35 @@ void test_RtlGetCallersAddress(){
 }
 
 void test_RtlInitAnsiString(){
-    /* FIXME: This is a stub! implement this function! */
+    const char* func_num = "0x0121";
+    const char* func_name = "RtlInitAnsiString";
+    BOOL tests_passed = 1;
+    ANSI_STRING ansi_string;
+    CHAR string[] = "Xbox";
+
+    print_test_header(func_num, func_name);
+
+    ansi_string.Length = 100;
+    ansi_string.MaximumLength = 200;
+    RtlInitAnsiString(&ansi_string, NULL);
+    tests_passed &= assert_ansi_string(
+        &ansi_string,
+        0,
+        0,
+        NULL,
+        "Src str = NULL, Length = 0"
+    );
+
+    RtlInitAnsiString(&ansi_string, string);
+    tests_passed &= assert_ansi_string(
+        &ansi_string,
+        strlen(string),
+        strlen(string) + 1,
+        string,
+        "Use char array to create ansi string"
+    );
+
+    print_test_footer(func_num, func_name, tests_passed);
 }
 
 void test_RtlInitUnicodeString(){
