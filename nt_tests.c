196d25a3bfb4435c4273838a14049d67bd667458
Luca D'Amico <damico.luca91@live.it>
2018-03-02 14:06:48 +0100

    Added test for NtCreateFile

--- nt_tests.c
+++ nt_tests.c
@@ -1,3 +1,6 @@
+#include <hal/fileio.h>
+#include "output.h"
+
 void test_NtAllocateVirtualMemory(){
     /* FIXME: This is a stub! implement this function! */
 }
@@ -23,7 +26,40 @@ void test_NtCreateEvent(){
 }
 
 void test_NtCreateFile(){
-    /* FIXME: This is a stub! implement this function! */
+    const char* func_num = "0x00BE";
+    const char* func_name = "NtCreateFile";
+    HANDLE handle;
+    NTSTATUS status;
+    OBJECT_ATTRIBUTES obj;
+    IO_STATUS_BLOCK isb;
+    ANSI_STRING obj_name;
+    char filepath[200];
+    BOOL tests_passed = 1;
+
+    print_test_header(func_num, func_name);
+
+    XConvertDOSFilenameToXBOX("./default.xbe", filepath);
+    RtlInitAnsiString(&obj_name, filepath);
+
+    obj.RootDirectory = NULL;
+    obj.Attributes = OBJ_CASE_INSENSITIVE;
+    obj.ObjectName = &obj_name;
+
+    status = NtCreateFile(
+    &handle,
+    GENERIC_READ,
+    &obj,
+    &isb,
+    NULL,
+    FILE_ATTRIBUTE_NORMAL,
+    0,
+    FILE_OPEN,
+    FILE_SYNCHRONOUS_IO_NONALERT);
+
+    tests_passed &= NT_SUCCESS(status);
+    NtClose(handle);
+
+    print_test_footer(func_num, func_name, tests_passed);
 }
 
 void test_NtCreateIoCompletion(){
