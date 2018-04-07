64c8d6c0b0fa5e6a8981df80f4439c270e64d0ba
Fisherman166 <Seankoppenhafer@gmail.com>
2018-04-07 09:37:39 -0700

    Make RtlCompareMemory tests actually check that all tests passed

--- rtl_tests.c
+++ rtl_tests.c
@@ -167,23 +167,23 @@ void test_RtlCompareMemory(){
     VOID* num2_ptr = (VOID*)&num2;
 
     SIZE_T bytes_matching = RtlCompareMemory(num1_ptr, num2_ptr, 0);
-    assert_rtl_compared_bytes(bytes_matching, 0, "Check 0 bytes of memory");
+    tests_passed &= assert_rtl_compared_bytes(bytes_matching, 0, "Check 0 bytes of memory");
 
     bytes_matching = RtlCompareMemory(num1_ptr, num2_ptr, sizeof(num1));
-    assert_rtl_compared_bytes(bytes_matching, sizeof(num1), "Both nums 0, should match");
+    tests_passed &= assert_rtl_compared_bytes(bytes_matching, sizeof(num1), "Both nums 0, should match");
 
     num1 |= 0x80000000;
     bytes_matching = RtlCompareMemory(num1_ptr, num2_ptr, sizeof(num1));
-    assert_rtl_compared_bytes(bytes_matching, sizeof(num1) - 1, "Only 3 bytes should match");
+    tests_passed &= assert_rtl_compared_bytes(bytes_matching, sizeof(num1) - 1, "Only 3 bytes should match");
 
     num2 |= 0x4000;
     bytes_matching = RtlCompareMemory(num1_ptr, num2_ptr, sizeof(num1));
-    assert_rtl_compared_bytes(bytes_matching, sizeof(num1) - 3, "Only 1 byte should match");
+    tests_passed &= assert_rtl_compared_bytes(bytes_matching, sizeof(num1) - 3, "Only 1 byte should match");
 
     num2 |= 0x80000000;
     num1 |= 0x4000;
     bytes_matching = RtlCompareMemory(num1_ptr, num2_ptr, sizeof(num1));
-    assert_rtl_compared_bytes(bytes_matching, sizeof(num1), "All bytes should match again");
+    tests_passed &= assert_rtl_compared_bytes(bytes_matching, sizeof(num1), "All bytes should match again");
 
     print_test_footer(func_num, func_name, tests_passed);
 }
