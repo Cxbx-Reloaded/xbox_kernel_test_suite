ff4549772c613958fbbe38939148b4abcf025d62
Luca D'Amico <damico.luca91@live.it>
2018-04-10 00:01:56 +0200

    avoid system crash in Av Tests

--- av_tests.c
+++ av_tests.c
@@ -4,37 +4,50 @@
 
 
 void test_AvGetSavedDataAddress(){
-    if(!is_emu){
-         print("0x0001 - AvGetSavedDataAddress: Skipped (due to real hadrware) / value: %x",AvGetSavedDataAddress());
-         return;
-    }
-    AvSetSavedDataAddress((void*) 0x12345678);
-    AvGetSavedDataAddress()==(void*)0x12345678 ? print("0x0001 - AvGetSavedDataAddress: Good") : print("0x0001 - AvGetSavedDataAddress: Faulty");
+    // FIXME this test is broken - We shouldn't pass NULL to AvSetSavedDataAddress. This is just an hack to avoid a system crash.
+    const char* func_num = "0x0001";
+    const char* func_name = "AvGetSavedDataAddress";
+    BOOL tests_passed = 1;
+    print_test_header(func_num, func_name);
+
+    AvSetSavedDataAddress((void*) NULL);
+    tests_passed = AvGetSavedDataAddress()==(void*) NULL ? 1 : 0;
+    print("AvGetSavedDataAddress()=0x%x", AvGetSavedDataAddress());
+
+    print_test_footer(func_num, func_name, tests_passed);
 }
 
 void test_AvSendTVEncoderOption(){
-    /* FIXME: there are other functions such as AV_OPTION_QUERY_MODE, AV_QUERY_ENCODER_TYPE, AV_OPTION_WIDESCREEN etc*/
+    // FIXME: there are other functions such as AV_OPTION_QUERY_MODE, AV_QUERY_ENCODER_TYPE, AV_OPTION_WIDESCREEN etc
+    // FIXME: this test is broken. I get inconsistent value from my real xbox
+    const char* func_num = "0x0002";
+    const char* func_name = "AvSendTVEncoderOption";
+    BOOL tests_passed = 1;
+    print_test_header(func_num, func_name);
+
     unsigned long res = 0;
-    AvSendTVEncoderOption(NULL, 6, 0, &res);
-    print("0x0002 - AvSendTVEncoderOption: %d (0=AV_PACK_NONE 1=AV_PACK_STANDARD 2=AV_PACK_RFU 3=AV_PACK_SCART 4=AV_PACK_HDTV 5=AV_PACK_VGA 6=AV_PACK_SVIDEO)", res);
+    AvSendTVEncoderOption((void *)NV2A_MMIO_BASE, 6, 0, &res);
+    print("AvSendTVEncoderOption: %d (0=AV_PACK_NONE 1=AV_PACK_STANDARD 2=AV_PACK_RFU 3=AV_PACK_SCART 4=AV_PACK_HDTV 5=AV_PACK_VGA 6=AV_PACK_SVIDEO)", (int)res);
+
+    print_test_footer(func_num, func_name, tests_passed);
 }
 
 void test_AvSetDisplayMode(){
-    if(!is_emu){
-         print("0x0003 - AvSetDisplayMode: Skipped (due to real hadrware)");
-         return;
-    }
-    // FATAL - Fails on emulator as of april 6th, 2018
+    // FIXME: FATAL - Fails on emulator as of april 6th, 2018
+    // FIXME: FATAL - Fails on real hardware too. The parameters passed to AvSetDisplayMode are wrong.
     return;
     AvSetDisplayMode(NULL, 0, 0, 0, 0, 0) == 0L ? print("0x0003 - AvSetDisplayMode: 0 (Good)") : print("0x0003 - AvSetDisplayMode: !=0 (Faulty)");
-
 }
 
 void test_AvSetSavedDataAddress(){
-    if(!is_emu){
-         print("0x0004 - AvSetSavedDataAddress: Skipped (due to real hadrware)");
-         return;
-    }
-    AvSetSavedDataAddress((void*) 0x12345678);
-    AvGetSavedDataAddress()==(void*)0x12345678 ? print("0x0004 - AvSetSavedDataAddress: Good") : print("0x0004 - AvSetSavedDataAddress: Faulty");
+    // FIXME this test is broken - We shouldn't pass NULL to AvSetSavedDataAddress. This is just an hack to avoid a system crash.
+    const char* func_num = "0x0004";
+    const char* func_name = "AvSetSavedDataAddress";
+    BOOL tests_passed = 1;
+    print_test_header(func_num, func_name);
+
+    AvSetSavedDataAddress((void*) NULL);
+    tests_passed = AvGetSavedDataAddress()==(void*) NULL ? 1 : 0;
+
+    print_test_footer(func_num, func_name, tests_passed);
 }
--- global.h
+++ global.h
@@ -3,6 +3,8 @@
 
 #include "vector.h"
 
+#define NV2A_MMIO_BASE 0xFD000000
+
 extern int is_emu;
 extern vector tests_to_run;
 
