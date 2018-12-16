81e56a57f6a9451c93215cc8d3f694e0e843a4a9
Fisherman166 <Seankoppenhafer@gmail.com>
2018-12-16 10:30:40 -0800

    Add test for RtlExtendedIntegerMultiply

--- rtl_tests.c
+++ rtl_tests.c
@@ -14,6 +14,9 @@
 #define STATUS_BUFFER_OVERFLOW      0x80000005
 #define STATUS_BUFFER_TOO_SMALL     0xC0000023
 
+const char* failed_text = "FAILED";
+const char* passed_text = "PASSED";
+
 void test_RtlAnsiStringToUnicodeString(){
     const char* func_num = "0x0104";
     const char* func_name = "RtlAnsiStringToUnicodeString";
@@ -645,7 +648,28 @@ void test_RtlEqualUnicodeString(){
 }
 
 void test_RtlExtendedIntegerMultiply(){
-    /* FIXME: This is a stub! implement this function! */
+    const char* func_num = "0x0119";
+    const char* func_name = "RtlExtendedIntegerMultiply";
+    BOOL tests_passed = 1;
+    print_test_header(func_num, func_name);
+
+    const LONGLONG multiplicands[]    = {1,  0,  15,   15, -15};
+    const LONG multipliers[]          = {0, -1,  15,  -15, -15};
+    const LONGLONG expected_results[] = {0,  0, 225, -225, 225};
+    LARGE_INTEGER multiplicand, result;
+
+    for(uint8_t i = 0; i < sizeof(expected_results) / sizeof(LONGLONG); i++) {
+        const char* result_text = passed_text;
+        multiplicand.QuadPart = multiplicands[i];
+        result = RtlExtendedIntegerMultiply(multiplicand, multipliers[i]);
+        if(result.QuadPart != expected_results[i]) {
+            tests_passed = 0;
+            result_text = failed_text;
+        }
+        print("  Test %s: Expected = %d for multiplicand = %d and multipler = %d, result = %d",
+              result_text, (LONG)expected_results[i], (LONG)multiplicands[i], (LONG)multipliers[i], (LONG)result.QuadPart);
+    }
+    print_test_footer(func_num, func_name, tests_passed);
 }
 
 void test_RtlExtendedLargeIntegerDivide(){
