ed4741e4d49732def264c75c1d96d5227a838903
RadWolfie <RadWolfie@users.noreply.github.com>
2021-03-31 10:41:19 -0500

    Add tests for IoCreateSymbolicLink and IoDeleteSymbolicLink

--- io_tests.c
+++ io_tests.c
@@ -1,3 +1,9 @@
+#include <xboxkrnl/xboxkrnl.h>
+#include <windows.h>
+
+#include "output.h"
+#include "common_assertions.h"
+
 void test_IoAllocateIrp(){
     /* FIXME: This is a stub! implement this function! */
 }
@@ -31,7 +37,43 @@ void test_IoCreateFile(){
 }
 
 void test_IoCreateSymbolicLink(){
-    /* FIXME: This is a stub! implement this function! */
+    const char* func_num = "0x0043";
+    const char* func_name = "IoCreateSymbolicLink";
+    BOOL tests_passed = 1;
+    print_test_header(func_num, func_name);
+
+    ANSI_STRING symlink_str, symlink_bad_str, device_str, device_w_slash_str, device_bad_path_nonexistent_str;
+    RtlInitAnsiString(&symlink_str, "\\xkts");
+    RtlInitAnsiString(&symlink_bad_str, "\\xkts\\bad_symlink");
+    RtlInitAnsiString(&device_str, "\\Device\\Harddisk0\\Partition5");
+    RtlInitAnsiString(&device_w_slash_str, "\\Device\\Harddisk0\\Partition5\\");
+    RtlInitAnsiString(&device_bad_path_nonexistent_str, "\\Device\\Harddisk0\\Partition5\\xkts_nonexistent");
+    NTSTATUS result;
+
+    result = IoCreateSymbolicLink(&symlink_str, &device_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_SUCCESS, func_name);
+
+    // Do not delete symbolic link yet in order to trigger object name collision error.
+    result = IoCreateSymbolicLink(&symlink_str, &device_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_OBJECT_NAME_COLLISION, func_name);
+
+    IoDeleteSymbolicLink(&symlink_str);
+
+    result = IoCreateSymbolicLink(&symlink_str, &device_w_slash_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_SUCCESS, func_name);
+
+    IoDeleteSymbolicLink(&symlink_str);
+
+    result = IoCreateSymbolicLink(&symlink_str, &device_bad_path_nonexistent_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_OBJECT_NAME_NOT_FOUND, func_name);
+
+    result = IoCreateSymbolicLink(&symlink_bad_str, &device_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_OBJECT_PATH_NOT_FOUND, func_name);
+
+    result = IoCreateSymbolicLink(&device_bad_path_nonexistent_str, &device_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_OBJECT_PATH_NOT_FOUND, func_name);
+
+    print_test_footer(func_num, func_name, tests_passed);
 }
 
 void test_IoDeleteDevice(){
@@ -39,7 +81,25 @@ void test_IoDeleteDevice(){
 }
 
 void test_IoDeleteSymbolicLink(){
-    /* FIXME: This is a stub! implement this function! */
+    const char* func_num = "0x0045";
+    const char* func_name = "IoDeleteSymbolicLink";
+    BOOL tests_passed = 1;
+    print_test_header(func_num, func_name);
+
+    ANSI_STRING symlink_str, device_str;
+    RtlInitAnsiString(&symlink_str, "\\xkts");
+    RtlInitAnsiString(&device_str, "\\Device\\Harddisk0\\Partition5");
+    NTSTATUS result;
+
+    IoCreateSymbolicLink(&symlink_str, &device_str);
+
+    result = IoDeleteSymbolicLink(&symlink_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_SUCCESS, func_name);
+
+    result = IoDeleteSymbolicLink(&symlink_str);
+    tests_passed &= assert_NTSTATUS(result, STATUS_OBJECT_NAME_NOT_FOUND, func_name);
+
+    print_test_footer(func_num, func_name, tests_passed);
 }
 
 void test_IoDeviceObjectType(){
