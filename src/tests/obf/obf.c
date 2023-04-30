310e970f75b49f1922c3100ab0220f20f1a72cab
RadWolfie <RadWolfie@users.noreply.github.com>
2023-04-30 09:56:17 +0000

    split Obf prefix functions

--- src/tests/obf/obf.c
+++
@@ -1,7 +0,0 @@
-void test_ObfDereferenceObject(){
-    /* FIXME: This is a stub! implement this function! */
-}
-
-void test_ObfReferenceObject(){
-    /* FIXME: This is a stub! implement this function! */
-}
