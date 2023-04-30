1d02e0aa039c9ad3b258526078da0fb607c833d5
RadWolfie <RadWolfie@users.noreply.github.com>
2023-04-30 09:36:40 +0000

    split Ki prefix functions

--- src/tests/ki/KiUnlockDispatcherDatabase.c
+++ src/tests/ki/KiUnlockDispatcherDatabase.c
@@ -1,6 +1,6 @@
-void test_KiBugCheckData(){
-    /* FIXME: This is a stub! implement this function! */
-}
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
 
 void test_KiUnlockDispatcherDatabase(){
     /* FIXME: This is a stub! implement this function! */
