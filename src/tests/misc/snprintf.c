a59f04a713a022b2a0eaa02e46104601da019985
RadWolfie <RadWolfie@users.noreply.github.com>
2023-04-30 09:45:10 +0000

    split misc functions

---
+++ src/tests/misc/snprintf.c
@@ -0,0 +1,7 @@
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
+
+void test__snprintf(){
+    /* FIXME: This is a stub! implement this function! */
+}
---
+++ src/tests/misc/sprintf.c
@@ -0,0 +1,7 @@
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
+
+void test__sprintf(){
+    /* FIXME: This is a stub! implement this function! */
+}
