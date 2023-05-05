af66370473ded88d9c7f0dd1421a4b8ef7b11bbb
RadWolfie <RadWolfie@users.noreply.github.com>
2023-05-05 06:39:07 -0500

    create dpc suite stub

--- src/tests/ke/KeRemoveQueueDpc.c
+++
@@ -1,8 +0,0 @@
-#include <xboxkrnl/xboxkrnl.h>
-
-#include "util/output.h"
-
-void test_KeRemoveQueueDpc()
-{
-    /* FIXME: This is a stub! implement this function! */
-}
---
+++ src/tests/ke/suite/dpc.c
@@ -0,0 +1,23 @@
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
+
+void test_KeInitializeDpc()
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+void test_KeInsertQueueDpc()
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+void test_KeIsExecutingDpc()
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+void test_KeRemoveQueueDpc()
+{
+    /* FIXME: This is a stub! implement this function! */
+}
