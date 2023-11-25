e415d6653e7aafde6e2f22d8ac83e4030e9e3f0e
ergo720 <45463469+ergo720@users.noreply.github.com>
2023-11-25 19:10:11 +0100

    Use GEN_CHECK macro, moved some tests to MmFreeSystemMemory

--- src/tests/mm/MmFreeSystemMemory.c
+++ src/tests/mm/MmFreeSystemMemory.c
@@ -1,8 +1,34 @@
 #include <xboxkrnl/xboxkrnl.h>
 
 #include "util/output.h"
+#include "assertions/defines.h"
 
 void test_MmFreeSystemMemory()
 {
-    /* FIXME: This is a stub! implement this function! */
+    const char* func_num = "0x00AC";
+    const char* func_name = "MmFreeSystemMemory";
+    BOOL test_passed = 1;
+    print_test_header(func_num, func_name);
+
+    // NOTE: these tests could be marked as failed if not enough memory can be allocated for them
+
+    ULONG num_of_pages;
+
+    // Test #1 - free one page
+    PVOID sys_addr = MmAllocateSystemMemory(PAGE_SIZE, PAGE_READONLY);
+    GEN_CHECK(sys_addr != NULL, TRUE, "SystemMemoryAddress (RO - PAGE_SIZE)");
+    if (sys_addr) {
+        num_of_pages = MmFreeSystemMemory(sys_addr, 0);
+        GEN_CHECK(num_of_pages, 1, "num_of_pages (RO - PAGE_SIZE)");
+    }
+
+    // Test #2 - free two pages
+    sys_addr = MmAllocateSystemMemory(PAGE_SIZE + 1, PAGE_READONLY);
+    GEN_CHECK(sys_addr != NULL, TRUE, "SystemMemoryAddress (RO - PAGE_SIZE + 1)");
+    if (sys_addr) {
+        num_of_pages = MmFreeSystemMemory(sys_addr, 0);
+        GEN_CHECK(num_of_pages, 2, "num_of_pages (RO - PAGE_SIZE + 1)");
+    }
+
+    print_test_footer(func_num, func_name, test_passed);
 }
