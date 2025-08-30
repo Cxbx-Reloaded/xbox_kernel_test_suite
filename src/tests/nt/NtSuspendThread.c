8806dfac356cf3817e330cb90242b2dc6b49a958
RadWolfie <RadWolfie@users.noreply.github.com>
2025-08-30 18:19:48 +0000

    create thread stub for Nt APIs

--- src/tests/nt/NtSuspendThread.c
+++
@@ -1,8 +0,0 @@
-#include <xboxkrnl/xboxkrnl.h>
-
-#include "util/output.h"
-
-TEST_FUNC(NtSuspendThread)
-{
-    /* FIXME: This is a stub! implement this function! */
-}
---
+++ src/tests/nt/suite/thread.c
@@ -0,0 +1,18 @@
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
+
+TEST_FUNC(NtQueueApcThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(NtResumeThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(NtSuspendThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
