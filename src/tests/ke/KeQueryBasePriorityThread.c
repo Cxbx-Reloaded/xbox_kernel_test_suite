b6c2e91d4e97b8f50fa6eb8c25158e85d816ec5f
RadWolfie <RadWolfie@users.noreply.github.com>
2025-08-30 18:26:27 +0000

    create thread stub for Ke APIs

--- src/tests/ke/KeQueryBasePriorityThread.c
+++
@@ -1,8 +0,0 @@
-#include <xboxkrnl/xboxkrnl.h>
-
-#include "util/output.h"
-
-TEST_FUNC(KeQueryBasePriorityThread)
-{
-    /* FIXME: This is a stub! implement this function! */
-}
---
+++ src/tests/ke/suite/thread.c
@@ -0,0 +1,63 @@
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
+
+TEST_FUNC(KeAlertResumeThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeAlertThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeBoostPriorityThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeDelayExecutionThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeGetCurrentThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeQueryBasePriorityThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeResumeThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeSetBasePriorityThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeSetDisableBoostThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeSetPriorityThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeSuspendThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeTestAlertThread)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
